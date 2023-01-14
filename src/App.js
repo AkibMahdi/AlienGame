51c68dd6fc49a4946e3366f1030cde9c706c3f60
Akib <akibmahdi1@gmail.com>
2023-01-14 03:19:17 -0500

    added objects and functions

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
+import * as Auth0 from 'auth0-web';
+
+Auth0.configure({
+  domain: 'dev-ksv3e53yc2rw00cp.us.auth0.com',
+  clientID: 'VyPG7Ftm73zLdd2xR7oMekhJAJVl8tY0',
+  redirectUri: 'http://localhost:3000/',
+  responseType: 'token id_token',
+  scope: 'openid profile manage:points',
+});
 
 class App extends Component {
   componentDidMount() {
     const self = this;
+    Auth0.handleAuthCallback();
+    Auth0.subscribe((auth) => {
+      console.log(auth);
+    });
     setInterval(() => {
         self.props.moveObjects(self.canvasMousePosition);
     }, 10);
@@ -24,16 +37,31 @@ class App extends Component {
   render() {
     return (
       <Canvas
-        angle={this.props.angle}
-        trackMouse={event => (this.trackMouse(event))}
-      />
+      angle={this.props.angle}
+      gameState={this.props.gameState}
+      startGame={this.props.startGame}
+      trackMouse={event => (this.trackMouse(event))}
+    />
     );
   }
 }
 
 App.propTypes = {
   angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+    flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+      position: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired
+      }).isRequired,
+      id: PropTypes.number.isRequired,
+    })).isRequired,
+  }).isRequired,
   moveObjects: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
 };
 
 export default App;
